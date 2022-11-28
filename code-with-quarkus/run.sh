47d495b9b3f2b3fab1bc0ff2c6bc1035fb98c5cd
rosenstaykov <rosenstaykov@users.noreply.github.com>
2022-11-28 16:16:17 +0200

    Update run.sh

--- code-with-quarkus/run.sh
+++ code-with-quarkus/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl -Ls https://sh.jbang.dev | bash -s - trust add https://repo1.maven.org/maven2/io/quarkus/quarkus-cli/
 curl -Ls https://sh.jbang.dev | bash -s - app install --fresh --force quarkus@quarkusio
-quarkus dev
+
